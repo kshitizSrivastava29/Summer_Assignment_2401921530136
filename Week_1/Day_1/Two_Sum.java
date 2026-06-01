169067fa7816b56340e22a83f994229b6e6f3257
Kshitiz Srivastava <24113cn077@glbitm.ac.in>
2026-06-01 20:24:58 +0530

    Implement twoSum function using HashMap

--- Week_1/Day_1/Two_Sum.java
+++ Week_1/Day_1/Two_Sum.java
@@ -1,1 +1,18 @@
-
+import java.util.*;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       int idx1 = -1;
+       int idx2 = -1;
+       HashMap<Integer,Integer> map = new HashMap<>();
+      for(int i = 0 ; i < nums.length ; i++){
+        if(map.containsKey(target-nums[i])){
+            idx1 = map.get(target-nums[i]);
+            idx2 = i;
+        }
+        map.put(nums[i],i);
+        
+      }
+      int ans[] = {idx1,idx2};
+      return ans; 
+    }
+}
