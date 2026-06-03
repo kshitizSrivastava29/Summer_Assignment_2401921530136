aeb48dc2b40bd12ea3ce77f4858a5c6962a435de
Kshitiz Srivastava <24113cn077@glbitm.ac.in>
2026-06-03 08:52:20 +0530

    Implement moveZeroes method to shift zeros

---
+++ Week_1/Day_3 : Two Pointer /Moves_Zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+       int sIdx = 0;
+       for(int i = 0 ; i < nums.length ; i++){
+        if(nums[i] != 0){
+            int temp = nums[sIdx];
+            nums[sIdx] = nums[i];
+            nums[i] = temp;
+            sIdx++;
+        }
+       } 
+    }
+}
