eb0956f4389811049e936f21667059c0b45092ac
Kshitiz Srivastava <24113cn077@glbitm.ac.in>
2026-06-07 10:51:40 +0530

    Implement first unique character finder in string

---
+++ Week_2/Day_1 : Hashing in Strings /First_Unique_Character_in_a_String.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] ch = new int[26];
+        for(char c : s.toCharArray()){
+            ch[c-'a']++;
+        }
+        for(int i = 0 ; i < s.length() ; i++){
+            char c = s.charAt(i);
+            if(ch[c-'a'] == 1)return i; 
+        }
+        return -1;
+    }
+}
