c0b15e7e0082f34acbecc1c420042d0eede60642
Kshitiz Srivastava <24113cn077@glbitm.ac.in>
2026-06-15 11:45:56 +0530

    Add palindrome check for singly-linked list
    Implemented a method to check if a linked list is a palindrome by using a two-pointer technique and reversing the second half of the list.

---
+++ Week_3/Day_2/Palindrome_LinkedList.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        // reverse
+        ListNode prev = slow;
+        ListNode curr = slow.next;
+        while(curr != null){
+            ListNode nextNode = curr.next;
+
+            curr.next = prev;
+
+            prev = curr;
+            curr = nextNode;
+        }
+        slow.next = null;
+        ListNode head2 = prev;
+
+
+        ListNode curr1 = head;
+        ListNode curr2 = head2;
+        while(curr1 != null && curr2 != null){
+            if(curr1.val != curr2.val)return false;
+            curr1 = curr1.next;
+            curr2 = curr2.next;
+        }
+        return true;
+    }
+}
